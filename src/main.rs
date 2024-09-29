e5b79b178108777cfff80a352e2aa99b90c1cde5
Sanket <sanket143@users.noreply.github.com>
2024-09-29 23:53:07 +0530

    remove winit and use tao

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,49 @@
-use dpi::{LogicalPosition, LogicalSize};
-use gtk::builders::WindowBuilder;
-use winit::{
+use tao::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
-use wry::{Rect, WebViewBuilder};
+use wry::WebViewBuilder;
 
 fn main() -> wry::Result<()> {
-    #[cfg(any(
-        target_os = "linux",
-        target_os = "dragonfly",
-        target_os = "freebsd",
-        target_os = "netbsd",
-        target_os = "openbsd",
-    ))]
-    {
-        use gtk::prelude::DisplayExtManual;
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-        gtk::init().unwrap();
-        if gtk::gdk::Display::default().unwrap().backend().is_wayland() {
-            panic!("This example doesn't support wayland!");
-        }
+    let builder = {
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+        let vbox = window.default_vbox().unwrap();
+        WebViewBuilder::new_gtk(vbox)
+    };
 
-        // we need to ignore this error here otherwise it will be catched by winit and will be
-        // make the example crash
-        winit::platform::x11::register_xlib_error_hook(Box::new(|_display, error| {
-            let error = error as *mut x11_dl::xlib::XErrorEvent;
-            (unsafe { (*error).error_code }) == 170
-        }));
-    }
-
-    let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new()
-        .with_inner_size(winit::dpi::LogicalSize::new(800, 800))
-        .build(&event_loop)
-        .unwrap();
-
-    let webview = WebViewBuilder::new_as_child(&window)
-        .with_url("https://tauri.app")
-        .build()?;
-
-    event_loop
-        .run(move |event, evl| {
-            evl.set_control_flow(ControlFlow::Poll);
-
-            while gtk::events_pending() {
-                gtk::main_iteration_do(false);
-            }
-
-            match event {
-                Event::WindowEvent {
-                    event: WindowEvent::Resized(size),
-                    ..
-                } => {
-                    let size = size.to_logical::<u32>(window.scale_factor());
-                    webview
-                        .set_bounds(Rect {
-                            position: LogicalPosition::new(0, 0).into(),
-                            size: LogicalSize::new(size.width, size.height).into(),
-                        })
-                        .unwrap();
+    let _webview = builder
+        .with_url("http://tauri.app")
+        .with_drag_drop_handler(|e| {
+            match e {
+                wry::DragDropEvent::Enter { paths, position } => {
+                    println!("DragEnter: {position:?} {paths:?} ")
                 }
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    ..
-                } => evl.exit(),
+                wry::DragDropEvent::Over { position } => println!("DragOver: {position:?} "),
+                wry::DragDropEvent::Drop { paths, position } => {
+                    println!("DragDrop: {position:?} {paths:?} ")
+                }
+                wry::DragDropEvent::Leave => println!("DragLeave"),
                 _ => {}
             }
-        })
-        .unwrap();
 
-    Ok(())
+            true
+        })
+        .build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit
+        }
+    });
 }
